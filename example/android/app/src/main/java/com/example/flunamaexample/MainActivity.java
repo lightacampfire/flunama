887ce67bcf16343c7a8677cdc845147699aeb449
tomgekeerd <tomderuiter.hhw@hotmail.com>
2018-12-31 14:35:45 +0100

    Removing more access tokens

--- example/android/app/src/main/java/com/example/flunamaexample/MainActivity.java
+++ example/android/app/src/main/java/com/example/flunamaexample/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Mapbox.getInstance(this, "pk.eyJ1IjoiZm9udGluYWxpcyIsImEiOiJjamo3N3QxZzkwcGZkM3Jzd3R0OHd3eG9jIn0.MDmSPZyfbQ7NP0toOzSnyg");
+    Mapbox.getInstance(this, "MAPBOX_ACCESS_TOKEN");
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
